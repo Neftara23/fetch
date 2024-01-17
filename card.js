60799b5b4c597849843c98228fea1957ef52facc
Neftara23 <neftali.aragonmoreno@gmail.com>
2024-01-17 15:13:10 -0600

    First commit

---
+++ card.js
@@ -0,0 +1,5 @@
+const IMAGE_URL = 'https://placehold.com/200';
+const IMAGE_CONTAINER = document.querySelector('.img-container');
+const INFO_CONTAINER = document.querySelector('.info-container');
+
+
